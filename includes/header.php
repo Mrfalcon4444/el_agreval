7133257835f6fbc4e80663991d59c7e1242e9dd3
Mrfalcon4444 <einar.falcon6619@alumnos.udg.mx>
2025-03-23 11:58:20 -0600

    nuevo favicon actualizado en formato

--- includes/header.php
+++ includes/header.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($pageTitle) ? $pageTitle : 'El Agreval'; ?></title>
 
-    <link rel="icon" type="image/png" href="/imagenes/logo.png"/>
+    <link rel="icon" type="image/svg+xml" href="imagenes/favicon.svg" />
     <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     
