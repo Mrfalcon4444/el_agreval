e9d49e2ca1c8e8c7708ba8d33d180dc0a7dd9b00
Mrfalcon4444 <einar.falcon6619@alumnos.udg.mx>
2025-03-23 11:48:22 -0600

    agregar favicon

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($pageTitle) ? $pageTitle : 'El Agreval'; ?></title>
-    
+
+    <link rel="icon" type="image/png" href="imagenes/logo.png"/>
     <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     
