ba0b55d49d0ebba3ff0434d61b7b02bb00ee9aa0
valen725 <juana.martinez@utp.edu.co>
2025-03-23 21:15:24 -0600

    depuracion

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -2,10 +2,15 @@
 // Iniciar sesión
 session_start();
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if (!isset($_SESSION['loggedin']) || $_SESSION['cargo'] != 'Administrador') {
     header("Location: ../login.php");
     exit();
 }
+
 
 require_once '../config/config.php';
 
