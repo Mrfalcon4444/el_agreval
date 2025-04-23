e6934c88f145bb8ab3e19f00a4318d14098bed16
Mrfalcon4444 <einar.falcon6619@alumnos.udg.mx>
2025-04-23 12:11:01 -0600

    Update simple_correo.php

--- mail/simple_correo.php
+++ mail/simple_correo.php
@@ -8,7 +8,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // Destinatario - CAMBIA ESTO
-$destinatario = 'tuemail@ejemplo.com';
+$destinatario = 'einarfalcon@gmail.com';
 
 // Mensaje simple
 $asunto = 'Mensaje de prueba';
