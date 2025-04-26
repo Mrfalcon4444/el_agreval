50d35db76159b461a80db56fb378a2c9c3eba30b
valen725 <juana.martinez@utp.edu.co>
2025-04-26 00:18:07 -0600

    Esperemos que ya se vea lindo

--- login.php
+++ login.php
@@ -48,12 +48,12 @@ include 'includes/header.php';
           </div>
         </div>
 
-        <!-- Campo de contraseña -->
         <div class="form-control w-full">
           <label class="label">
             <span class="label-text">Contraseña</span>
           </label>
           <div class="relative">
+            <!-- Ícono de la llave a la izquierda -->
             <span class="absolute inset-y-0 left-0 flex items-center pl-3">
               <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                 <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
@@ -61,8 +61,10 @@ include 'includes/header.php';
               </svg>
             </span>
 
-            <input type="password" name="contraseña" id="password" required placeholder="Contraseña" class="input input-bordered w-full pl-10 pr-10" />
-            
+            <!-- Campo de contraseña -->
+            <input type="password" name="contraseña" id="password" required placeholder="Contraseña" class="input input-bordered w-full pl-10 pr-10 bg-white" />
+
+            <!-- Botón del ojito a la derecha -->
             <button type="button" id="togglePassword" class="absolute inset-y-0 right-0 flex items-center pr-3">
               <svg id="eyeIcon" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
