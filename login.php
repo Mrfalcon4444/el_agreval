5847a7d5fbf6f7075a862430cdbbfc9936d6d031
valen725 <juana.martinez@utp.edu.co>
2025-04-26 00:11:40 -0600

    ojitoooo

--- login.php
+++ login.php
@@ -37,8 +37,9 @@ include 'includes/header.php';
             <span class="label-text">Correo Electrónico</span>
           </label>
           <div class="relative">
-            <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-              <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+          <label class="input validator">
+            <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none" stroke="currentColor">
                 <rect width="20" height="16" x="2" y="4" rx="2"></rect>
                 <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
               </svg>
