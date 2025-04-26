7a29bb92c0f68fae21d06a3738d26ce5d388d181
valen725 <juana.martinez@utp.edu.co>
2025-04-26 00:29:27 -0600

    c

--- login.php
+++ login.php
@@ -53,9 +53,10 @@ include 'includes/header.php';
             <span class="label-text">Contraseña</span>
           </label>
           <div class="relative">
+          <label class="input validator">
             <!-- Ícono de la llave a la izquierda -->
             <span class="absolute inset-y-0 right-0 flex items-center pl-3">
-              <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+              <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5">
                 <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
                 <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
               </svg>
