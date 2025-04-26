d4f0920bdeec8ba7f1c8c1befb38eb5711c4828a
valen725 <juana.martinez@utp.edu.co>
2025-04-26 00:21:50 -0600

    Revert "x"
    This reverts commit 4e657780f5e401e3b9ef7e9c5256f754e4710c9e.

--- login.php
+++ login.php
@@ -37,17 +37,16 @@ include 'includes/header.php';
             <span class="label-text">Correo Electrónico</span>
           </label>
           <div class="relative">
-            <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-              <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none" stroke="currentColor">
-                  <rect width="20" height="16" x="2" y="4" rx="2"></rect>
-                  <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-                </g>
+          <label class="input validator">
+            <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none" stroke="currentColor">
+                <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
               </svg>
             </span>
             <input type="email" name="correo" placeholder="correo@ejemplo.com" required class="input input-bordered w-full pl-10" />
           </div>
-
+        </div>
 
         <div class="form-control w-full">
           <label class="label">
