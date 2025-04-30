86012ffe3a4a66480b6f873e93a05027b2cd6bdd
Mrfalcon4444 <einar.falcon6619@alumnos.udg.mx>
2025-04-30 11:40:35 -0600

    descripción para google

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Sistema de gestión para El Agreval - Una plataforma para administración de recursos humanos dedicado a escuelas.">
     <title><?php echo isset($pageTitle) ? $pageTitle : 'El Agreval'; ?></title>
 
     <link rel="icon" type="image/png" href="imagenes/favicon-96x96.png" sizes="96x96" />
